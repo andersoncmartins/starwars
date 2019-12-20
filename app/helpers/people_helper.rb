f6407984bece3f0191774b8c33af7c471361bf9c
Anderson Cardoso Martins <cardosomartins.anderson@gmail.com>
2019-12-20 16:36:36 -0300

    Removendo codigo sujo e melhorando exibição da descrição.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -2,6 +2,10 @@ module PeopleHelper
 
   def description(person)
     starship_names = person.starships.map{|s| s.name }.join(" e ")
-    "Olá! Meu nome é #{person.name}. Sou um #{I18n.t("species.#{person.specie_name}")}, peso #{person.mass} Kg, nasci em #{person.planet_name} no ano de #{person.birth_year} e já pilotei #{starship_names}."
+    if starship_names.blank?
+      "Olá! Meu nome é #{person.name}. Sou um #{I18n.t("species.#{person.specie_name}")}, peso #{person.mass} Kg, nasci em #{person.planet_name} no ano de #{person.birth_year} e não pilotei nenhuma nave."  
+    else
+      "Olá! Meu nome é #{person.name}. Sou um #{I18n.t("species.#{person.specie_name}")}, peso #{person.mass} Kg, nasci em #{person.planet_name} no ano de #{person.birth_year} e já pilotei #{starship_names}."
+    end
   end
 end
